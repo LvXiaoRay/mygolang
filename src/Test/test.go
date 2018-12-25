d90b5d65bcc1ebf45e27ab4a2e98dcecfb96abda
LvXiaoRay <15256021201@163.com>
2018-12-25 10:36:46 +0800

    测试golang import

---
+++ src/Test/test.go
@@ -0,0 +1,7 @@
+package Test
+
+import "fmt"
+
+func Test4() {
+	fmt.Println("success")
+}
---
+++ src/impoot.go
@@ -0,0 +1,7 @@
+package main
+
+import "Test" //fuck,,,,,,引用的包名必须要 首字母大写，
+
+func main() {
+	Test.Test4() //方法也要首字母大写
+}
